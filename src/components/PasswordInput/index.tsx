9ccc55f50726b77ea6213b9c0ed128c227888fe1
alexxuss1996 <alekseymelnych132@gmail.com>
2023-08-27 20:51:26 +0300

    Refactor Password input

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -1,24 +1,26 @@
 import { useRef, useState } from "react";
+import { FaCopy } from "react-icons/fa";
 
 const PasswordInput = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const [isWeakPassword, setWeakPassword] = useState<boolean>(true);
+  const copyButtonRef = useRef<HTMLButtonElement | null>(null);
+  const [isValidPassword, setIsValidPassword] = useState<boolean>(false);
 
-  const emptyHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const PasswordInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length > 5) {
-      setWeakPassword(false);
+      setIsValidPassword(true);
     } else {
-      setWeakPassword(true);
+      setIsValidPassword(false);
     }
   };
 
-  async function copyToClipBoard() {
+  const copyToClipBoard = () => {
     if (inputRef.current) {
       const password = inputRef.current?.value;
-      await navigator.clipboard.writeText(password);
+      navigator.clipboard.writeText(password);
       alert("Copied to clipboard");
     }
-  }
+  };
   return (
     <div className="h-20 bg-light-dark border-none text-white px-5 mb-8 flex items-center justify-between">
       <input
@@ -26,14 +28,15 @@ const PasswordInput = () => {
         className="bg-light-dark font-bold text-4xl text-white pl-5 placeholder:text-gray inline-block"
         placeholder="P4S$W0rD!"
         ref={inputRef}
-        onChange={emptyHandler}
+        onChange={PasswordInputHandler}
       />
       <button
         className="bg-light-dark text-white px-4 py-2 rounded-md"
-        disabled={isWeakPassword}
+        disabled={!isValidPassword}
+        ref={copyButtonRef}
         onClick={copyToClipBoard}
       >
-        Copy to Clipboard
+        <FaCopy color={copyButtonRef.current?.disabled ? "#A4FFAF" : "#807D90"} size={36} />
       </button>
     </div>
   );
