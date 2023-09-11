2ed8985808bec6614f3586027bbae9e8970234cd
alexxuss1996 <alekseymelnych132@gmail.com>
2023-09-11 17:32:43 +0300

    Add types for PasswordSettings and response

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,11 @@
 export type PasswordStrength = "bad" | "weak" | "medium" | "strong";
+export type PasswordSettingsType = {
+  includeLowercase: boolean;
+  includeNumbers: boolean;
+  includeSpecialCharacters: boolean;
+  includeUppercase: boolean;
+};
+
+export type APIResponseType = {
+  password: string;
+}
